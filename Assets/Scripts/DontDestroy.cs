9a9980a70c8cf083e00f73978b643bd82a75b3b2
Alex Nilsson <alexnilsson2006@gmail.com>
2025-11-07 14:44:19 +0100

    Sound Script

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -3,16 +3,45 @@ using UnityEngine;
 public class DontDestroy : MonoBehaviour
 {
     public string tag;
+
+    float volume = 1;
+    AudioSource audioSource;
     
     void Start()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag(tag);
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(transform.tag);
 
         if (objs.Length > 1)
         {
+            objs[0].GetComponent<DontDestroy>().StartFake();
+
             Destroy(this.gameObject);
         }
 
+
+        audioSource = FindObjectOfType<AudioSource>();
+
+        if(audioSource != null)
+        {
+
+            audioSource.volume = volume;
+
+        }
+
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void StartFake()
+    {
+
+        audioSource = FindObjectOfType<AudioSource>();
+
+        if (audioSource != null)
+        {
+
+            audioSource.volume = volume;
+
+        }
+
     }
 }
